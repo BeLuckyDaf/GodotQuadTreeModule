c2d4bda3653dc25b29913af91317e7392a3d0498
Vladislav Smirnov <beluc@yandex-team.ru>
2023-02-17 13:07:48 +0300

    Fix header guard

--- register_types.h
+++ register_types.h
@@ -1,4 +1,9 @@
+#ifndef QUADTREE_REGISTER_TYPES_H
+#define QUADTREE_REGISTER_TYPES_H
+
 #include "modules/register_module_types.h"
 
 void initialize_quadtree_module(ModuleInitializationLevel p_level);
 void uninitialize_quadtree_module(ModuleInitializationLevel p_level);
+
+#endif
